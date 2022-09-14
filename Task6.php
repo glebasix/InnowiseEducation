b29e0489785907ebdf8a15927a2a348977c821c7
glebasix <glebasix@gmail.com>
2022-09-14 17:21:23 +0300

    finish task 6

--- Task6.php
+++ Task6.php
@@ -4,33 +4,33 @@ namespace src;
 
 class Task6
 {
+    /**
+     * @param int    $year
+     * @param int    $lastYear
+     * @param int    $month
+     * @param int    $lastMonth
+     * @param string $day
+     *
+     * @return int
+     */
     public function main(int $year, int $lastYear, int $month, int $lastMonth, string $day = 'Monday'): int
     {
-        $firstDate = new \DateTime();
+        if ($lastYear < 0 || $lastYear > $year) {
+            return throw new \InvalidArgumentException();
+        }
+
+        $Date = new \DateTime();
         $lastDate = new \DateTime();
-        $firstDate->setDate($year, $month, 1);
+        $Date->setDate($year, $month, 1);
         $lastDate->setDate($lastYear, $lastMonth, 1);
         $counter = 0;
-        while ($firstDate <= $lastDate) {
-            if ($firstDate->format('l') == $day) {
+        while ($lastDate <= $Date) {
+            if ($lastDate->format('l') == $day) {
                 $counter++;
             }
-            $firstDate->modify('+1 month');
+            $lastDate->modify('+1 month');
         }
 
         return $counter;
     }
-}
-
-$test = new Task6;
-
-try {
-    $example = $test->main(2020, 2022, 1, 12);
-    if ($example) {
-        echo $example;
-    } else {
-        throw new \TypeError();
-    }
-} catch(\TypeError $e) {
-    echo $e->getMessage();
 }
