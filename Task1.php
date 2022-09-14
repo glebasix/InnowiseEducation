cd86bcd73618143c4d921a701ad893ca44fbda13
glebasix <glebasix@gmail.com>
2022-09-14 16:59:04 +0300

    finish task 1

--- Task1.php
+++ Task1.php
@@ -4,33 +4,22 @@ namespace src;
 
 class Task1
 {
+    /**
+     * @param int $inputNumber
+     *
+     * @return string
+     */
     public function main(int $inputNumber): string
     {
-        if ($inputNumber > 10) {
-            if ($inputNumber > 20) {
-                if ($inputNumber > 30) {
-                    return 'More than 30';
-                }
-
-                return 'More than 20';
-            }
-
-            return 'More than 10';
-        } else {
-            return 'Equal or less than 10';
+        if (!is_int($inputNumber)) {
+            return throw new \InvalidArgumentException();
         }
-    }
-}
 
-$test = new Task1;
+        $result = ($inputNumber > 30) ? 'More than 30' :
+            ($inputNumber > 20 ? 'More than 20' :
+                ($inputNumber > 10 ? 'More than 10' :
+                    'Equal or less than 10'));
 
-try {
-    $example = $test->main('24');
-    if ($example) {
-        echo $example;
-    } else {
-        throw new \TypeError();
+        return $result;
     }
-} catch(\TypeError $e) {
-    echo $e->getMessage();
 }
