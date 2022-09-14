f342e20834ba61e3884221faaa07798f27fdfc8f
glebasix <glebasix@gmail.com>
2022-09-14 18:01:15 +0300

    finish task 1

--- Task1.php
+++ Task1.php
@@ -11,10 +11,6 @@ class Task1
      */
     public function main(int $inputNumber): string
     {
-        if (!is_int($inputNumber)) {
-            return throw new \InvalidArgumentException();
-        }
-
         $result = ($inputNumber > 30) ? 'More than 30' :
             ($inputNumber > 20 ? 'More than 20' :
                 ($inputNumber > 10 ? 'More than 10' :
