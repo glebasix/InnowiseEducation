e1187fc086e6d6c5e48f390e9b2a94c3a2dc3cde
glebasix <glebasix@gmail.com>
2022-09-14 17:15:13 +0300

    finish task 3

--- Task3.php
+++ Task3.php
@@ -4,25 +4,21 @@ namespace src;
 
 class Task3
 {
+    /**
+     * @param int $number
+     *
+     * @return int
+     */
     public function main(int $number): int
     {
+        if (!is_int($number) || $number < 0) {
+            return throw new \InvalidArgumentException();
+        }
+
         while ($number >= 10) {
             $number = array_sum((str_split((string)$number)));
         }
 
         return $number;
     }
-}
-
-$test = new Task3;
-
-try {
-    $example = $test->main('7778');
-    if ($example) {
-        echo $example;
-    } else {
-        throw new \TypeError();
-    }
-} catch(\TypeError $e) {
-    echo $e->getMessage();
 }
