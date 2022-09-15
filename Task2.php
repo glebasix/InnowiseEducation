5e3e03ecbf44619088fc9c968255fe0ec387eff9
glebasix <glebasix@gmail.com>
2022-09-15 16:38:17 +0300

    finish task 2

--- Task2.php
+++ Task2.php
@@ -11,8 +11,14 @@ class Task2
      */
     public function main(string $date): int
     {
+        if (!is_string($date)) {
+            return throw new \InvalidArgumentException();
+        }
         $now = new \DateTime();
         $birthdayDate = \DateTime::createFromFormat('d-m-Y', $date);
+        if ($birthdayDate < $now) {
+            $birthdayDate->modify('+1 year');
+        }
         $interval = $now->diff($birthdayDate);
 
         return $interval->days;
