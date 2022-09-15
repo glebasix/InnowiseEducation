1e80a530467ab5a612662f21382921a1ff961b70
glebasix <glebasix@gmail.com>
2022-09-15 16:40:05 +0300

    finish task 5

--- Task5.php
+++ Task5.php
@@ -11,24 +11,21 @@ class Task5
      */
     public function main(int $n): string
     {
-        if ($n <= 0) {
+        if (!is_int($n) || $n < 0) {
             return throw new \InvalidArgumentException();
         }
-
         $number1 = '0';
         $number2 = '1';
-        $inHead = '0';
         $counter = 2;
         do {
             for ($j = 0; $j < $counter; $j++) {
+                $inHead = '0';
+                $result = '';
                 $number1_count = strlen($number1 = strrev($number1));
                 $number2_count = strlen($number2 = strrev($number2));
-
                 $zeroFill = str_repeat('0', abs($number2_count - $number1_count));
                 ($number1_count < $number2_count) ? $number1 .= $zeroFill : $number2 .= $zeroFill;
                 $count = strlen($number2);
-
-
                 for ($i = 0; $i < $count; $i++) {
                     $numberSum = $number1[$i] + $number2[$i] + $inHead;
                     $result .= ($numberSum > 9) ? $numberSum - 10 : $numberSum;
@@ -38,7 +35,6 @@ class Task5
                 $number1 = $number2;
                 $number2 = $finalFibonacci;
                 unset($result, $inHead);
-
                 $totalLength = strlen($finalFibonacci);
                 if ($n == $totalLength) {
                     break 2;
